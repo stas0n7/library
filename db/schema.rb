c20dd8607ed1341d33300cad57ef89676625c134
stas0n7 <r14war@gmail.com>
2020-03-02 12:35:43 +0200

    add CarrieWave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_01_130549) do
+ActiveRecord::Schema.define(version: 2020_03_02_101557) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_03_01_130549) do
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "cover"
   end
 
   create_table "categories", force: :cascade do |t|
