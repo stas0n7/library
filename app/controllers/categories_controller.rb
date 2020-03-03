4390b1ed406c779961c61355e266c7a383feab42
stas0n7 <r14war@gmail.com>
2020-03-04 00:20:25 +0200

    add book_add and book remove to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :set_category, only: %i[show edit update destroy add_book_page add_book remove_book_page remove_book]
 
   # GET /categories
   # GET /categories.json
@@ -19,6 +19,22 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1/edit
   def edit
+  end
+
+  def add_book_page
+  end
+
+  def remove_book_page
+  end
+
+  def add_book
+    @category.books << Book.find(params[:book_id])
+    redirect_to categories_path, notice: 'book successfully added'
+  end
+
+  def remove_book
+    @category.books.delete Book.find(params[:book_id])
+    redirect_to categories_path, notice: 'book successfully removed'
   end
 
   # POST /categories
