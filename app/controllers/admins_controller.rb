739a82bb60e68d0604f7d27d18ea5672c2a28c83
stas0n7 <r14war@gmail.com>
2020-03-05 12:28:15 +0200

    complete role managment

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,15 +1,26 @@
 class AdminsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, except: :show
+  before_action :admin_check
   def show
     @users = User.all
   end
 
   def edit
-    @user = User.find(params[:user_id])
   end
 
   def update
-    @user = User.find(params[:user_id])
     @user.update(role: params[:role])
     redirect_to admin_path if @user.save
+  end
+
+  private
+
+  def admin_check
+    redirect_to root_path, notice: 'only admin have access' unless current_user.role == 'admin'
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
