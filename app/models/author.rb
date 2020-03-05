831979d3817be77be5371654ef5a0bc3c8073db9
stas0n7 <r14war@gmail.com>
2020-03-05 17:05:13 +0200

    add borwsers validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,7 @@
 class Author < ApplicationRecord
   has_many :authors_books
   has_many :books, through: :authors_books
+
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   has_many :books
+
   validates :title, presence: true
+  validates :title, uniqueness: true
 
   def outside_books
     Book.all - books
